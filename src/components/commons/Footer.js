50d3d024dcc737e1138d97ddfc93c30832b8fcab
dkile <dkile532@naver.com>
2022-09-13 15:28:22 +0900

    Paste footer element to Footer component

--- src/components/commons/Footer.js
+++ src/components/commons/Footer.js
@@ -1,5 +1,22 @@
 const Footer = () => {
-  return;
+  return (
+    <footer>
+      <div className="footer-top">
+        <div className="footer-top-nav">
+          <div className="footer-top-nav-main">
+            <img src="" alt="" />
+          </div>
+          <div className="footer-top-nav-sns"></div>
+          <div className="footer-top-nav-info"></div>
+        </div>
+        <div className="footer-top-description"></div>
+      </div>
+      <div className="footer-bottom">
+        <img src="" alt=""></img>
+        <div className="footer-bottom-copyright"></div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
