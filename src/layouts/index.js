f0822cd789198c8307e3863977f37609055ddcc9
dkile <dkile532@naver.com>
2022-09-13 15:32:49 +0900

    Change footer element to Footer component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,23 +1,10 @@
+import { Footer } from "../components/commons";
+
 const MainLayout = (props) => {
   return (
     <div>
       <body>{props.childeren}</body>
-      <footer>
-        <div className="footer-top">
-          <div className="footer-top-nav">
-            <div className="footer-top-nav-main">
-              <img src="" alt="" />
-            </div>
-            <div className="footer-top-nav-sns"></div>
-            <div className="footer-top-nav-info"></div>
-          </div>
-          <div className="footer-top-description"></div>
-        </div>
-        <div className="footer-bottom">
-          <img src="" alt=""></img>
-          <div className="footer-bottom-copyright"></div>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 };
