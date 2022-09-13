81ca6cc992bfaead6b1dfb028dc13df62f74ba1c
dkile <dkile532@naver.com>
2022-09-13 16:55:01 +0900

    Create header frame in Header component

---
+++ src/components/commons/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <img src="" alt="" />
+        <div className="adult-toggle"></div>
+      </div>
+      <div className="header-novel-nav"></div>
+      <div className="header-profile-nav"></div>
+    </header>
+  );
+};
+
+export default Header;
