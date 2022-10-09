6a1970c4aa61de47777edd86f5e3dae110186abc
dkile <dkile532@naver.com>
2022-10-09 23:52:42 +0900

    Create json file for s3 assets

---
+++ src/config/assets.js
@@ -0,0 +1,85 @@
+const assets = {
+  contest: {
+    label: "공모전",
+    imgSrc: "",
+  },
+  free: {
+    label: "자유",
+    imgSrc: "",
+  },
+  plus: {
+    label: "플러스",
+    imgSrc: "",
+  },
+  rank: {
+    label: "랭킹",
+    imgSrc: "",
+  },
+  mybook: {
+    label: "내서재",
+    imgSrc: "",
+  },
+  alarm: {
+    label: "알람",
+    imgSrc:
+      "https://novelpia-clone.s3.ap-northeast-2.amazonaws.com/image/alarm.png",
+  },
+  writerRoom: {
+    label: "내작품",
+    imgSrc:
+      "https://novelpia-clone.s3.ap-northeast-2.amazonaws.com/image/write.png",
+  },
+  search: {
+    label: "검색",
+    imgSrc:
+      "https://novelpia-clone.s3.ap-northeast-2.amazonaws.com/image/search.png",
+  },
+  myInformation: {
+    label: "내정보",
+    imgSrc:
+      "https://novelpia-clone.s3.ap-northeast-2.amazonaws.com/image/none_user.png",
+  },
+
+  plusShop: {
+    label: "플러스샵",
+    imgSrc:
+      "https://novelpia-clone.s3.ap-northeast-2.amazonaws.com/image/icon_svc_plus_shop.png",
+  },
+  audio: {
+    label: "보이스북",
+    imgSrc:
+      "https://novelpia-clone.s3.ap-northeast-2.amazonaws.com/image/icon_svc_voice.png",
+  },
+  laboratory: {
+    label: "실험실",
+    imgSrc:
+      "https://novelpia-clone.s3.ap-northeast-2.amazonaws.com/image/icon_svc_laboratory.png",
+  },
+  prime: {
+    label: "프라임",
+    imgSrc:
+      "https://novelpia-clone.s3.ap-northeast-2.amazonaws.com/image/icon_svc_prime.png",
+  },
+  atelier: {
+    label: "아틀리에",
+    imgSrc:
+      "https://novelpia-clone.s3.ap-northeast-2.amazonaws.com/image/icon_svc_atelier2.png",
+  },
+  attendance: {
+    label: "출석판",
+    imgSrc:
+      "https://novelpia-clone.s3.ap-northeast-2.amazonaws.com/image/icon_svc_attendance.png",
+  },
+  arena: {
+    label: "아레나",
+    imgSrc:
+      "https://novelpia-clone.s3.ap-northeast-2.amazonaws.com/image/icon_svc_arena.png",
+  },
+  new: {
+    label: "",
+    imgSrc:
+      "https://novelpia-clone.s3.ap-northeast-2.amazonaws.com/image/icon_svc_new.png",
+  },
+};
+
+export default assets;
