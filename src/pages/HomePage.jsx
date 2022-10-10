6a72d104dcc701df76a331ea693d03ecf7bd853d
dkile <dkile532@naver.com>
2022-10-10 11:17:53 +0900

    Insert components in homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,20 @@
+import Banner from "../components/commons/Banner";
+import RealtimeHot from "../components/Home/RealtimeHot";
+import ServiceNav from "../components/Home/ServiceNav";
+import TopicNovelList from "../components/Novel/TopicNovelList";
+import Layout from "../layouts";
+
 const HomePage = () => {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <Banner />
+      <ServiceNav />
+      <TopicNovelList />
+      <RealtimeHot />
+      <TopicNovelList />
+      <TopicNovelList />
+    </Layout>
+  );
 };
 
 export default HomePage;
